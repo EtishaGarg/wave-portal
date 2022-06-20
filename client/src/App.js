5891aeab6252c94f31223b6e39aa0cdbb960f7ba
Etisha Garg <gargetisha@gmail.com>
2022-06-20 14:14:20 +0530

    Wave portal front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,106 @@
+import React from "react";
+import {ethers} from "ethers";
+import abi from "./utils/WavePortal.json";
 import './App.css';
 
 function App() {
+
+  const [currentAccount, setCurrentAccount] = React.useState("");
+  const contractAddress = "0x2a95f5C619630A50B0a596e6010bc899725e549E"
+  const contractABI = abi.abi;
+
+  const CheckIfWalletIsConnected = async() => {
+    try {
+      const {ethereum} = window;
+
+      if(!ethereum) {
+        console.log("Make sure you have Metamask!");
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts"});
+
+      if(accounts.length != 0){
+        console.log("Found an authorized account: ",accounts[0]);
+        setCurrentAccount(accounts[0]);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  React.useEffect(() => {
+    CheckIfWalletIsConnected()
+  },[])
+
+  const connectWallet = async() => {
+    try {
+      const {ethereum} = window;
+
+      if(!ethereum){
+        alert("Get Metamask!");
+      }
+        const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+        console.log("Account connected ",accounts[0]);
+        setCurrentAccount(accounts[0]);
+      
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  const wave = async() => {
+
+    try {
+      const {ethereum} = window;
+      
+      if(ethereum){
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retreived total wave count...", count.toNumber());
+
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining ",waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined ",waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retreived total wave count...", count.toNumber());
+
+      }
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div className="App">
-      <h1>Wave Portal</h1>
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">
+          👋 Hey there!
+        </div>
+
+        <div className="bio">
+          I'm Etisha! Welcome to my wave portal. How are you doing today?
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+
+        {!currentAccount && 
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        }
+      </div>
     </div>
   );
 }
