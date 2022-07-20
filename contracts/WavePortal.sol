9e2d1eeaa0b9b93151b9d1d41bfe2776e0f8bc0d
Etisha Garg <gargetisha@gmail.com>
2022-07-20 13:06:27 +0530

    updated contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,6 +8,8 @@ contract WavePortal {
 
     uint public totalWaves;
 
+    uint private seed;
+
     event newWave(address indexed from, uint timestamp, string message);
 
     struct Wave {
@@ -18,16 +20,35 @@ contract WavePortal {
 
     Wave[] waveArray;
 
-    constructor() {
+    mapping (address => uint) public lastWavedAt;
+    constructor () payable{
         console.log("Welcome to my wave-portal");
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory message) public {
+
+        require(lastWavedAt[msg.sender] + 15 minutes < block.timestamp, "Wait for 15 mins");
+
+        lastWavedAt[msg.sender] = block.timestamp;
         totalWaves = totalWaves + 1;
         waveArray.push(Wave(msg.sender, message, block.timestamp));
         console.log("%s has waved at %d with a message %s", msg.sender,block.timestamp,message);
 
-        emit newWave(msg.sender,block.timestamp,message);
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        console.log("Random # generated: %d", seed);
+
+        if(seed < 50) {
+            console.log("%s won!", msg.sender);
+            uint prizeAmount = 0.0001 ether;
+            require(prizeAmount <= address(this).balance, "Trying to withdraw more money than the contract has");
+            (bool success, ) = (msg.sender).call{value : prizeAmount}("");
+            require(success, "Failed to send Ether");
+
+        }
+
+        emit newWave(msg.sender,block.timestamp,message); 
     }
 
     function getAllWaves() public view returns(Wave[] memory){
